46f2906bac823ef6e0f3b346ecd5125ba3483aae
jhoon9494 <wjdgns9406@gmail.com>
2022-09-13 20:52:09 +0900

    Feat: mypage layout 제작

---
+++ src/components/organism/MenuBar.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import MenuBox from '../atoms/MenuBox';
+import UserImage from '../atoms/UserImage';
+import Logo from '../atoms/Logo';
+
+type ImgType = {
+  src: string;
+  alt: string;
+  name: string;
+};
+
+type LinkType = {
+  link: string;
+  text: string;
+};
+
+interface IProps {
+  img: ImgType;
+  links: LinkType[];
+}
+
+function MenuBar({ img, links }: IProps) {
+  // MenuBar의 default메뉴를 첫 번째 메뉴로 활성화하고, 각 메뉴 선택 시 해당 메뉴를 활성화시키는 코드
+  const location = useLocation();
+  const [currLinkIndex, setCurrLinkIndex] = useState<number>(0);
+
+  useEffect(() => {
+    links.forEach(({ link }, index) => {
+      if (location.pathname.includes(link)) {
+        setCurrLinkIndex(index);
+      }
+    });
+  }, [location, links]);
+
+  return (
+    <MenuBox>
+      <Logo />
+      <UserImage src={img.src} alt={img.alt} name={img.name} />
+      <LinkContainer>
+        {links.map(({ link, text }, idx) => {
+          return (
+            <Link to={link} key={`${link}-link`}>
+              <Text active={idx === currLinkIndex}>{text}</Text>
+            </Link>
+          );
+        })}
+      </LinkContainer>
+    </MenuBox>
+  );
+}
+
+export default MenuBar;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 57px;
+
+  > a {
+    margin-bottom: 32px;
+    text-align: center;
+  }
+`;
+
+const Text = styled.span<{ active: boolean }>`
+  font-size: 24px;
+  color: ${({ active }) => (active ? 'white' : '#979797')};
+`;
---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import MenuBar from '../../components/organism/MenuBar';
+
+function Mypage() {
+  // TODO 유저정보 api를 통해 받아온 후 전달
+  const userData = {
+    img: {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9mv9u2cfDIBp7Xjn0otKqsPtVMBoxyZtP6e2cxZ87hA&s',
+      alt: 'user',
+    },
+    name: '최정훈',
+    email: 'asd@asd.com',
+    phone: '01023232121',
+  };
+
+  const links = [
+    {
+      link: 'editProfile',
+      text: 'Edit Profile',
+    },
+    {
+      link: 'changePssword',
+      text: 'Password',
+    },
+    {
+      link: 'deleteAccount',
+      text: 'Delete Account',
+    },
+  ];
+
+  return (
+    <Container>
+      <MenuBar img={{ src: userData.img.src, alt: userData.img.alt, name: userData.name }} links={links} />
+      <Body>
+        <Outlet context={userData} />
+      </Body>
+    </Container>
+  );
+}
+
+export default Mypage;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 1280px;
+  margin: 0 auto;
+  display: flex;
+`;
+
+const Body = styled.div`
+  width: 1030px;
+  height: 100vh;
+`;
